470e6298730addaab648ddf830e3c69a700e0df8
OndrejHana <ondrahana001@gmail.com>
2023-02-09 16:47:10 +0100

    added lexical editor theming

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -5,6 +5,14 @@ import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
 
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { QuoteNode, HeadingNode } from "@lexical/rich-text";
+import { MarkdownShortcutPlugin } from "@lexical/react/LexicalMarkdownShortcutPlugin";
+import { TRANSFORMERS } from "@lexical/markdown";
+
+import { TableCellNode, TableNode, TableRowNode } from "@lexical/table";
+import { ListItemNode, ListNode } from "@lexical/list";
+import { CodeHighlightNode, CodeNode } from "@lexical/code";
+import { AutoLinkNode, LinkNode } from "@lexical/link";
 
 function Placeholder() {
   return <div className="editor-placeholder">Write here...</div>;
@@ -12,23 +20,35 @@ function Placeholder() {
 
 const initialConfig = {
   namespace: "TextEditor",
-  theme: {
-    ltr: "bg-blue-mid"
-  },
+  theme: customEditorTheme,
+  nodes: [
+    HeadingNode,
+    ListNode,
+    ListItemNode,
+    QuoteNode,
+    CodeNode,
+    CodeHighlightNode,
+    TableNode,
+    TableCellNode,
+    TableRowNode,
+    AutoLinkNode,
+    LinkNode
+  ],
   onError: (err: Error) => console.log(err),
 }
 
 export default function Editor() {
   return (
     <LexicalComposer initialConfig={initialConfig} >
-      <div className="editor-container">
+      <div className="editor-container  ">
         {/* <ToolbarPlugin/> */}
         <div className="editor-inner">
           <RichTextPlugin 
-            contentEditable={<ContentEditable className="editor-input" />}
+            contentEditable={<ContentEditable className="editor-input relative" />}
             placeholder={<Placeholder/>}
             ErrorBoundary={LexicalErrorBoundary}
           />
+          <MarkdownShortcutPlugin transformers={TRANSFORMERS} />
         </div>
       </div>
     </LexicalComposer>
--- src/pages/app/homepage.tsx
+++ src/pages/app/homepage.tsx
@@ -10,7 +10,7 @@ export function getServerSideProps(ctx: GetServerSidePropsContext) {
 
 export default function Homepage() {
   return (
-    <div>
+    <div className="bg-gray-darker">
       <Editor />
     </div>
   );
